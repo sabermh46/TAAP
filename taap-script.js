1abd738eace0eaff8f2a4a3384045cfa3d4dfb5b
Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2022-06-28 20:50:28 +0600

    ll

--- taap-script.js
+++ taap-script.js
@@ -2,6 +2,7 @@
 
 
 var cardsbtn = document.querySelectorAll('.collapseable-card > .card-controll > .buttons > .up-down');
+var crossbtns = document.querySelectorAll('.collapseable-card > .card-controll > .buttons > .cross');
 
 console.log(cardsbtn);
 cardsbtn.forEach((btn, i)=>{
@@ -9,5 +10,12 @@ cardsbtn.forEach((btn, i)=>{
     console.log(cardBody);
     btn.addEventListener('click', ()=>{
         cardBody.classList.toggle('active');
+    })
+})
+
+crossbtns.forEach((btn, i)=>{
+    var cardBody = document.querySelector(`.collapseable-card:nth-child(${i+2})`);
+    btn.addEventListener('click', ()=>{
+        cardBody.remove();
     })
 })

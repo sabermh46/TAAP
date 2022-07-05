c0eb0d1bdf63839cbd8e1696bf8aa6202738b989
Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2022-07-05 14:54:17 +0600

    Animation Smooth

--- taap-script.js
+++ taap-script.js
@@ -4,14 +4,39 @@
 var cardsbtn = document.querySelectorAll('.collapseable-card > .card-controll > .buttons > .up-down');
 var crossbtns = document.querySelectorAll('.collapseable-card > .card-controll > .buttons > .cross');
 
-console.log(cardsbtn);
+var totalBt = cardsbtn.length;
+var ht = [];
+var clicked = [];
+for(var j=0; j<=totalBt; j++)
+{
+    clicked[j] = 0;
+}
+var cB = document.querySelectorAll('.collapseable-card .card-body');
+    cB.forEach((body, i) =>{
+        var height = body.offsetHeight;
+        body.style.height = `${height}px`;
+        ht[i] = height;
+        console.log(ht[i]);
+    })
+
+
 cardsbtn.forEach((btn, i)=>{
     var cardBody = document.querySelector(`.collapseable-card:nth-child(${i+2})`);
-    console.log(cardBody);
+    var cBB = document.querySelector(`.collapseable-card:nth-child(${i+2}) > .card-body`);
+
+
     btn.addEventListener('click', ()=>{
         cardBody.classList.toggle('active');
+        if(++clicked[i]%2) {
+            cBB.style.height = '0px';
+        }
+        else {
+            cBB.style.height = `${ ht[i] }px`;
+        }
     })
 })
+
+
 
 crossbtns.forEach((btn, i)=>{
     var cardBody = document.querySelector(`.collapseable-card:nth-child(${i+2})`);
